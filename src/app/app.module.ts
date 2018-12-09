e241c5eaccad077c65b931d107cd23fafaa35a10
MOHAMMED SHINE N <shine160700@gmail.com>
2018-12-09 23:11:08 +0530

    first update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
+//app module is where we are importing and refencing all the components that are required to run the application
+//here the aap.module ts is converted to js code which is complide by the browser compiler.
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+//here we are importing the component to the app module or the main module
+import { HeaderComponent} from './common/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    //here we are refencing the header component to use it in the app component 
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/common/header/header.component.ts
@@ -0,0 +1,13 @@
+//here we are importing component module from @angular/core directory which is mandatory for all components 
+import { Component} from '@angular/core';
+
+
+@Component({
+    selector:'bwm-header',
+    templateUrl : "./header.component.html",
+    styleUrls : ["./header.component.scss"]
+})
+export class HeaderComponent{
+    
+
+}
